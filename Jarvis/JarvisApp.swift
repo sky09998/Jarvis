e19c962d3603ca08d201b1c1dc85fcfcf5defd9c
Sky Vittal <sky09@duck.com>
2025-08-19 10:31:23 -0700

    Initial Commit

---
+++ Jarvis/JarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisApp.swift
+//  Jarvis
+//
+//  Created by Chandramouli Vittal on 19/8/25.
+//
+
+import SwiftUI
+
+@main
+struct JarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JarvisTests/JarvisTests.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisTests.swift
+//  JarvisTests
+//
+//  Created by Chandramouli Vittal on 19/8/25.
+//
+
+import Testing
+@testable import Jarvis
+
+struct JarvisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
